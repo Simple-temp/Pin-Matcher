1b4954d0cf4d08f8e94e4caa0dd71bc8450e0e32
Simple-temp <fiveriam01@gmail..com>
2021-12-01 00:19:34 -1200

    100% done

--- js/index.js
+++ js/index.js
@@ -1,20 +1,26 @@
+/*---------------------get value-------------------------- */
 const numbers = [0,1,2,3,4,5,6,7,8,9];
 const generatePin = document.querySelector(".generatePin");
 const submitButton = document.querySelector(".submit-btn");
 const calButton = document.querySelectorAll(".button");
 const clearBtn = document.querySelector(".clear-btn");
-
-
+const backSpace = document.querySelector(".backSpace");
+/*---------------------get calculator pin number-------------------------- */
 for(let j=0 ; j<calButton.length ; j++)
 {
     calButton[j].addEventListener("click",function(){
 
-        document.querySelector("#calPin").value +=this.innerHTML;
+    document.querySelector("#calPin").value +=this.innerHTML;
 
     });
 }
-
-
+/*---------------------backspace button-------------------------- */
+backSpace.addEventListener("click",function(){
+    let change = document.querySelector("#calPin").value;
+    let newChange = change.substring(0, change.length - 1);
+    document.querySelector("#calPin").value = newChange;
+})
+/*---------------------get random 4 digit pin number-------------------------- */
 generatePin.addEventListener("click",function(){
 
     let RandomPin ='';
@@ -27,21 +33,28 @@ generatePin.addEventListener("click",function(){
     }
     const storeValue = RandomPin;
 
-    document.querySelector("#getpin").value=storeValue;
+        document.querySelector("#getpin").value=storeValue;
 
 })
-
-submitButton.addEventListener("click",function(calPin,getpin){
-       var b = calPin*getpin
-       console.log(b);
+/*---------------------submit value value-------------------------- */
+submitButton.addEventListener("click",function(){
+    const notifyTwo = document.querySelector(".notifyTwo");
+    const notifyOne = document.querySelector(".notifyOne");
+    if((document.querySelector("#calPin").value)==(document.querySelector("#getpin").value))
+    {
+        notifyTwo.style.display="block";
+    }
+    else
+    {
+        notifyOne.style.display="block";
+    }
 });
-
-
+/*---------------------random number function-------------------------- */
 function getRandomPinNumber()
 {
     return Math.floor ( Math.random() * numbers.length);
 }
-
+/*---------------------clear button-------------------------- */
 clearBtn.addEventListener("click",function(){
     document.querySelector("#calPin").value='';
 })   
